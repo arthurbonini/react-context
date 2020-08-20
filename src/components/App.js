3282001f6e3788f97f92e1e591ffbb2a7309b903
Arthur Bonini <arthur.bonini@prs-invivo.com>
2020-08-19 22:02:31 -0400

    build language selector

---
+++ src/components/App.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+class App extends React.Component {
+  state = { language: "english" };
+
+  onLanguageChange = (language) => {
+    this.setState({ language });
+  };
+
+  render() {
+    return (
+      <div className="ui container">
+        <div>
+          Select a Language:
+          <i
+            className="flag us"
+            onClick={() => this.onLanguageChange("english")}
+          ></i>
+          <i
+            className="flag nl"
+            onClick={() => this.onLanguageChange("dutch")}
+          ></i>
+        </div>
+        {this.state.language}
+      </div>
+    );
+  }
+}
+
+export default App;
