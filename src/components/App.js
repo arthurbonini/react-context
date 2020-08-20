1e59df52b48d4de4d8881b6057a07223c371c407
Arthur Bonini <arthur.bonini@prs-invivo.com>
2020-08-20 10:28:11 -0400

    move language selector to its own component

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 
+import LanguageSelector from "./LanugageSelector";
 import UserCreate from "./UserCreate";
 import LanguageContext from "../contexts/LanguageContext";
 import ColorContext from "../contexts/ColorContext";
@@ -14,18 +15,8 @@ class App extends React.Component {
   render() {
     return (
       <div className="ui container">
-        <div>
-          Select a Language:
-          <i
-            className="flag us"
-            onClick={() => this.onLanguageChange("english")}
-          ></i>
-          <i
-            className="flag nl"
-            onClick={() => this.onLanguageChange("dutch")}
-          ></i>
-        </div>
-        <ColorContext.Provider value="primary">
+        <LanguageSelector onLanguageChange={this.onLanguageChange} />
+        <ColorContext.Provider value="red">
           <LanguageContext.Provider value={this.state.language}>
             <UserCreate />
           </LanguageContext.Provider>
